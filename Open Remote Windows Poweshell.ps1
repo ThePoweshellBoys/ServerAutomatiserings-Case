72f0457ef9bac0f9c2d65bc33cb4fc73ad8750e7
ThePoweshellBoys <chri78k1@zbc.dk>
2025-09-05 08:12:34 +0200

    Update Open Remote Windows Poweshell.ps1

--- Open Remote Windows Poweshell.ps1
+++ Open Remote Windows Poweshell.ps1
@@ -2,18 +2,19 @@
 Enable-PSRemoting -Force
 
 # Opret en fjernsession til serveren
-$session = New-PSSession -ComputerName 10.10.30.2 -Credential (Get-Credential)
+$session = New-PSSession -ComputerName 10.101.232.173 -Credential (Get-Credential)
 
 
 # Luk sessionen
 Remove-PSSession $session
 
 # Tilføj Trusted Host
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value "ip"
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "10.101.232.173"
 
 # WINRM-Services
 Get-Service winrm
 winrm quickconfig
 
 #Turn ON/OFF Firewal
+
 Set-NetFirewallProfile -Enabled True
